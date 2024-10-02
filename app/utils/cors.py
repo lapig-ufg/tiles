383eddfd66eab79e3d235fae34372f7fc76f5892
Tharles de Sousa Andrade <irtharles@gmail.com>
2024-10-02 20:58:28 -0300

    Update CORS.

---
+++ app/utils/cors.py
@@ -0,0 +1,16 @@
+
+from os import getenv
+
+# Obtém o caminho do diretório dos arquivos estáticos via variável de ambiente
+origin_regex = r"^https:\/\/(?:\w+\.)?lapig\.iesa\.ufg\.br$|^https:\/\/lapig-ufg\.github\.io$"
+
+# Função para obter as origens separadas por vírgula da variável de ambiente
+def get_origins_from_env():
+    origins = getenv('ALLOW_ORIGINS', '')
+    if not origins:
+        return []  # Retorna lista vazia se a variável de ambiente não estiver definida ou estiver vazia
+    return [origin.strip() for origin in origins.split(',') if origin]
+
+
+# Obtém as origens permitidas da variável de ambiente
+allow_origins = get_origins_from_env()
--- main.py
+++ main.py
@@ -7,11 +7,11 @@ from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
 from google.oauth2 import service_account
 import valkey
-from os import getenv
 from fastapi.middleware.cors import CORSMiddleware
 from app.config import settings, logger, start_logger
 from app.database import Base, engine
 from app.router import created_routes
+from app.utils.cors import origin_regex, allow_origins
 
 Base.metadata.create_all(bind=engine)
 
@@ -22,20 +22,6 @@ class ORJSONResponse(JSONResponse):
         return orjson.dumps(content)
 
 app = FastAPI(default_response_class=ORJSONResponse)
-
-# Obtém o caminho do diretório dos arquivos estáticos via variável de ambiente
-origin_regex = r"^https:\/\/(?:\w+\.)?lapig\.iesa\.ufg\.br$|^https:\/\/lapig-ufg\.github\.io$"
-
-# Função para obter as origens separadas por vírgula da variável de ambiente
-def get_origins_from_env():
-    origins = getenv('ALLOW_ORIGINS', '')
-    if not origins:
-        return []  # Retorna lista vazia se a variável de ambiente não estiver definida ou estiver vazia
-    return [origin.strip() for origin in origins.split(',') if origin]
-
-
-# Obtém as origens permitidas da variável de ambiente
-allow_origins = get_origins_from_env()
 
 # Configurações CORS com expressões regulares para subdomínios dinâmicos
 app.add_middleware(
